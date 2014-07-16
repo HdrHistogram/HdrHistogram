91ded53f82e580f114bdcd6aa92367a41d3f34fa
Michael Barker <mikeb01@gmail.com>
2014-07-16 21:19:01 +1200

    [C] Lots of comments.

--- src/main/c/hdr_histogram_log.h
+++ src/main/c/hdr_histogram_log.h
@@ -31,13 +31,52 @@ struct hdr_log_writer
 {
 };
 
+/**
+ * Initialise the log writer.
+ *
+ * @param writer 'This' pointer
+ * @return 0 on success.
+ */
 int hdr_log_writer_init(struct hdr_log_writer* writer);
+
+/**
+ * Write the header to the log, this will constist of a user defined string,
+ * the current timestamp, version information and the CSV header.
+ *
+ * @param writer 'This' pointer
+ * @param file The stream to output the log header to.
+ * @param user_prefix User defined string to include in the header.
+ * @param timestamp The start time that the histogram started recording from.
+ * @return Will return 0 if it successfully completed or an error number if there
+ * was a failure.  EIO if the write failed.
+ */
 int hdr_log_write_header(
     struct hdr_log_writer* writer,
     FILE* file,
     const char* user_prefix,
     struct timespec* timestamp);
 
+/**
+ * Write an hdr_histogram entry to the log.  It will be encoded in a similar
+ * fashion to the approach used by the Java version of the HdrHistogram.  It will
+ * be a CSV line consisting of <start timestamp>,<end timestamp>,<max>,<histogram>
+ * where <histogram> is the binary histogram gzip compressed and base64 encoded.
+ *
+ * Timestamp is a bit of misnomer for the start_timestamp and end_timestamp values
+ * these could be offsets, e.g. start_timestamp could be offset from process start
+ * time and end_timestamp could actually be the length of the recorded interval.
+ *
+ * @param writer 'This' pointer
+ * @param file The stream to write the entry to.
+ * @param start_timestamp The start timestamp to include in the logged entry.
+ * @param end_timestamp The end timestamp to include in the logged entry.
+ * @param histogram The histogram to encode and log.
+ * @return Will return 0 if it successfully completed or an error number if there
+ * was a failure.  Errors include HDR_DEFLATE_INIT_FAIL, HDR_DEFLATE_FAIL if
+ * something when wrong during gzip compression.  ENOMEM if we failed to allocate
+ * or reallocate the buffer used for encoding (out of memory problem).  EIO if
+ * write failed.
+ */
 int hdr_log_write(
     struct hdr_log_writer* writer,
     FILE* file,
@@ -52,11 +91,52 @@ struct hdr_log_reader
     struct timespec start_timestamp;
 };
 
+/**
+ * Initialise the log reader.
+ *
+ * @param reader 'This' pointer
+ * @return 0 on success
+ */
 int hdr_log_reader_init(struct hdr_log_reader* reader);
+
+/**
+ * Reads the the header information from the log.  Will capure information
+ * such as version number and start timestamp from the header.
+ *
+ * @param hdr_log_reader 'This' pointer
+ * @param file The data stream to read from.
+ * @return 0 on success.  An error number on failure.
+ */
 int hdr_log_read_header(struct hdr_log_reader* reader, FILE* file);
+
+/**
+ * Reads an entry from the log filling in the specified histogram, timestamp and
+ * interval values.  If the supplied pointer to the histogram for this method is
+ * NULL then a new histogram will be allocated for the caller, however it will
+ * become the callers responsibility to free it later.  If the pointer is non-null
+ * the histogram read from the log will be merged with the supplied histogram.
+ *
+ * @param reader 'This' pointer
+ * @param file The stream to read the histogram from.
+ * @param histogram Pointer to allocate a histogram to or merge into.
+ * @param timestamp The first timestamp from the CSV entry.
+ * @param interval The second timestamp from the CSV entry
+ * @return Will return 0 on success or an error number if there was some wrong
+ * when reading in the histogram.  HDR_INFLATE_INIT_FAIL or HDR_INFLATE_FAIL if
+ * there was a problem with Gzip.  HDR_COMPRESSION_COOKIE_MISMATCH or
+ * HDR_ENCODING_COOKIE_MISMATCH if the cookie values are incorrect.
+ * HDR_LOG_INVALID_VERSION if the log can not be parsed.  ENOMEM if buffer space
+ * or the histogram can not be allocated.  EIO if there was an error during
+ * the read.  EINVAL in any input values are incorrect.
+ */
 int hdr_log_read(
     struct hdr_log_reader* reader, FILE* file, struct hdr_histogram** histogram,
     struct timespec* timestamp, struct timespec* interval);
 
-
+/**
+ * Returns a string representation of the error number.
+ *
+ * @param errnum The error response from a previous call.
+ * @return The user readable representation of the error.
+ */
 const char* hdr_strerror(int errnum);
