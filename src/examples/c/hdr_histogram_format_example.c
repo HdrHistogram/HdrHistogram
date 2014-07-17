797d41d6f546f8abdc224d5fa35b9925b2eb61d4
Michael Barker <mikeb01@gmail.com>
2014-07-18 10:46:21 +1200

    [C] Show error handling in example.

--- src/examples/c/hdr_histogram_format_example.c
+++ src/examples/c/hdr_histogram_format_example.c
@@ -26,6 +26,19 @@ int main(int argc, char **argv)
     hdr_record_value(raw_histogram, 100000000L);
     hdr_record_corrected_value(cor_histogram, 100000000L, 10000L);
 
-    hdr_percentiles_print(raw_histogram, stdout, 5, 1.0, CLASSIC);
+    int rc = 0;
+
+    rc = hdr_percentiles_print(raw_histogram, stdout, 5, 1.0, CLASSIC);
+    if (rc != 0)
+    {
+        printf("Failed to format raw_histogram: %s\n", strerror(rc));
+        return rc;
+    }
+
     hdr_percentiles_print(cor_histogram, stdout, 5, 1.0, CLASSIC);
+    if (rc != 0)
+    {
+        printf("Failed to format cor_histogram: %s\n", strerror(rc));
+        return rc;
+    }
 }
