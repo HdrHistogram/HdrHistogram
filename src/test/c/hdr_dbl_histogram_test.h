985503bebfc26837df81fe68b52d9a643866b15b
Michael Barker <mikeb01@gmail.com>
2015-01-06 15:23:15 +1300

    [C] Remove 'C' code, moved to HdrHistogram_c repo.

--- src/test/c/hdr_dbl_histogram_test.h
+++
@@ -1,6 +0,0 @@
-/**
- * hdr_dbl_histogram_test.h
- * Written by Michael Barker and released to the public domain,
- * as explained at http://creativecommons.org/publicdomain/zero/1.0/
- */
-int hdr_dbl_histogram_run_tests();
--- src/test/c/hdr_histogram_test.h
+++
@@ -1,7 +0,0 @@
-/**
- * hdr_histogram_test.h
- * Written by Michael Barker and released to the public domain,
- * as explained at http://creativecommons.org/publicdomain/zero/1.0/
- */
-
-int hdr_histogram_run_tests();
