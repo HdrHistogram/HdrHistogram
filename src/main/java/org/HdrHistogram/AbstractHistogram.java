7c8f7664e36735be1b9f3f4a1d756209161da872
Simon Cox <simon.cox@gmail.com>
2016-09-20 21:42:06 +0800

    Fix generation of garbage
    * Replace inadvertent boxing of double
    * This fixes HdrHistogram/HdrHistogram#113

--- src/main/java/org/HdrHistogram/AbstractHistogram.java
+++ src/main/java/org/HdrHistogram/AbstractHistogram.java
@@ -1267,7 +1267,7 @@ public abstract class AbstractHistogram extends AbstractHistogramBase implements
         recordedValuesIterator.reset();
         while (recordedValuesIterator.hasNext()) {
             HistogramIterationValue iterationValue = recordedValuesIterator.next();
-            Double deviation = (medianEquivalentValue(iterationValue.getValueIteratedTo()) * 1.0) - mean;
+            double deviation = (medianEquivalentValue(iterationValue.getValueIteratedTo()) * 1.0) - mean;
             geometric_deviation_total += (deviation * deviation) * iterationValue.getCountAddedInThisIterationStep();
         }
         double std_deviation = Math.sqrt(geometric_deviation_total / getTotalCount());
