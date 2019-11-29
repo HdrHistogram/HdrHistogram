d12a7b1568d8beee2bec103a89d202c7b70e9b08
Amir Hadadi <ahadadi@outbrain.com>
2019-11-29 10:27:24 +0200

    * Make StripedWriterReaderPhaser extend WriterReaderPhaser. * Add padding to prevent false sharing.

--- src/main/java/org/HdrHistogram/StripedWriterReaderPhaser.java
+++ src/main/java/org/HdrHistogram/StripedWriterReaderPhaser.java
@@ -10,93 +10,64 @@ import java.util.concurrent.atomic.AtomicLongArray;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * {@link StripedWriterReaderPhaser} instances provide an asymmetric means for synchronizing the execution of
- * wait-free "writer" critical sections against a "reader phase flip" that needs to make sure no writer critical
- * sections that were active at the beginning of the flip are still active after the flip is done. Multiple writers
- * and multiple readers are supported.
- * <p>
- * While a {@link StripedWriterReaderPhaser} can be useful in multiple scenarios, a specific and common use case is
- * that of safely managing "double buffered" data stream access in which writers can proceed without being
- * blocked, while readers gain access to stable and unchanging buffer samples
- * <blockquote>
- * NOTE: {@link StripedWriterReaderPhaser} writers are wait-free on architectures that support wait-free atomic
- * increment operations. They remain lock-free (but not wait-free) on architectures that do not support
- * wait-free atomic increment operations.
- * </blockquote>
- * {@link StripedWriterReaderPhaser} "writers" are wait free, "readers" block for other "readers", and
- * "readers" are only blocked by "writers" whose critical section was entered before the reader's
- * {@link StripedWriterReaderPhaser#flipPhase()} attempt.
- * <p>
- * When used to protect an actively recording data structure, the assumptions on how readers and writers act are:
- * <ol>
- * <li>There are two sets of data structures ("active" and "inactive")</li>
- * <li>Writing is done to the perceived active version (as perceived by the writer), and only
- *     within critical sections delineated by {@link StripedWriterReaderPhaser#writerCriticalSectionEnter}
- *     and {@link StripedWriterReaderPhaser#writerCriticalSectionExit}).</li>
- * <li>Only readers switch the perceived roles of the active and inactive data structures.
- *     They do so only while under readerLock(), and only before calling flipPhase().</li>
- * </ol>
- * When the above assumptions are met, {@link StripedWriterReaderPhaser} guarantees that the inactive data structures are not
- * being modified by any writers while being read while under readerLock() protection after a flipPhase()
- * operation.
- *
- *
+ * {@link StripedWriterReaderPhaser} is a striped version of {@link WriterReaderPhaser}
+ * which reduces contention at the expense of higher space consumption.
  *
  */
-public class StripedWriterReaderPhaser {
+public class StripedWriterReaderPhaser extends WriterReaderPhaser {
+    private static final int PADDING_BYTES = 128;
+    private static final int STRIDE = PADDING_BYTES / 8;
+    private static final int MAX_STRIPES = Integer.MAX_VALUE / STRIDE;
+
+    private static final int DEFAULT_NUMBER_OF_STRIPES = 8;
+
     private final int stripes;
 
-    // TODO: prevent false sharing
     private final AtomicLongArray startEpoch;
     private final AtomicLongArray evenEndEpoch;
     private final AtomicLongArray oddEndEpoch;
 
     private final ReentrantLock readerLock = new ReentrantLock();
 
+    /**
+     * Creates a {@link StripedWriterReaderPhaser} with the default number of stripes.
+     */
+    public StripedWriterReaderPhaser() {
+        this(DEFAULT_NUMBER_OF_STRIPES);
+    }
+
+    /**
+     * Creates a {@link StripedWriterReaderPhaser} with the specified number of stripes.
+     */
     public StripedWriterReaderPhaser(final int stripes) {
         if (stripes <= 0 || (stripes & (stripes - 1)) != 0) {
-            throw new IllegalArgumentException("Stripes must be a power of 2, not " + stripes);
+            throw new IllegalArgumentException("The number of stripes must be a power of 2, not " + stripes);
+        }
+        if (stripes > MAX_STRIPES) {
+            throw new IllegalArgumentException("There must be at most " + MAX_STRIPES + " stripes, not " + stripes);
         }
         this.stripes = stripes;
-        startEpoch = new AtomicLongArray(stripes);
-        evenEndEpoch = new AtomicLongArray(stripes);
-        oddEndEpoch = new AtomicLongArray(stripes);
+        final int paddedStripes = stripes * STRIDE;
+        startEpoch = new AtomicLongArray(paddedStripes);
+        evenEndEpoch = new AtomicLongArray(paddedStripes);
+        oddEndEpoch = new AtomicLongArray(paddedStripes);
         for (int i=0; i<stripes; i++) {
-            oddEndEpoch.set(i, Long.MIN_VALUE);
+            oddEndEpoch.set(i * STRIDE, Long.MIN_VALUE);
         }
     }
 
     /**
-     * Indicate entry to a critical section containing a write operation.
-     * <p>
-     * This call is wait-free on architectures that support wait free atomic increment operations,
-     * and is lock-free on architectures that do not.
-     * <p>
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} must be matched with a subsequent
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionExit(long)} in order for CriticalSectionPhaser
-     * synchronization to function properly.
-     *
-     * @return an (opaque) value associated with the critical section entry, which MUST be provided
-     * to the matching {@link StripedWriterReaderPhaser#writerCriticalSectionExit} call.
+     * {@inheritDoc}
      */
+    @Override
     public long writerCriticalSectionEnter() {
         return startEpoch.getAndIncrement(threadIndex());
     }
 
     /**
-     * Indicate exit from a critical section containing a write operation.
-     * <p>
-     * This call is wait-free on architectures that support wait free atomic increment operations,
-     * and is lock-free on architectures that do not.
-     * <p>
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionExit(long)} must be matched with a preceding
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} call, and must be provided with the
-     * matching {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} call's return value, in
-     * order for CriticalSectionPhaser synchronization to function properly.
-     *
-     * @param criticalValueAtEnter the (opaque) value returned from the matching
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} call.
+     * {@inheritDoc}
      */
+    @Override
     public void writerCriticalSectionExit(long criticalValueAtEnter) {
         if (criticalValueAtEnter < 0) {
             oddEndEpoch.incrementAndGet(threadIndex());
@@ -106,43 +77,25 @@ public class StripedWriterReaderPhaser {
     }
 
     /**
-     * Enter to a critical section containing a read operation (mutually excludes against other
-     * {@link StripedWriterReaderPhaser#readerLock} calls).
-     * <p>
-     * {@link StripedWriterReaderPhaser#readerLock} DOES NOT provide synchronization
-     * against {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} calls. Use {@link StripedWriterReaderPhaser#flipPhase()}
-     * to synchronize reads against writers.
+     * {@inheritDoc}
      */
+    @Override
     public void readerLock() {
         readerLock.lock();
     }
 
     /**
-     * Exit from a critical section containing a read operation (relinquishes mutual exclusion against other
-     * {@link StripedWriterReaderPhaser#readerLock} calls).
+     * {@inheritDoc}
      */
+    @Override
     public void readerUnlock() {
         readerLock.unlock();
     }
 
     /**
-     * Flip a phase in the {@link StripedWriterReaderPhaser} instance, {@link StripedWriterReaderPhaser#flipPhase()}
-     * can only be called while holding the readerLock().
-     * {@link StripedWriterReaderPhaser#flipPhase()} will return only after all writer critical sections (protected by
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} ()} and
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionExit(long)} ()}) that may have been in flight when the
-     * {@link StripedWriterReaderPhaser#flipPhase()} call were made had completed.
-     * <p>
-     * No actual writer critical section activity is required for {@link StripedWriterReaderPhaser#flipPhase()} to
-     * succeed.
-     * <p>
-     * However, {@link StripedWriterReaderPhaser#flipPhase()} is lock-free with respect to calls to
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} and
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionExit(long)}. It may spin-wait for for active
-     * writer critical section code to complete.
-     *
-     * @param yieldTimeNsec The amount of time (in nanoseconds) to sleep in each yield if yield loop is needed.
+     * {@inheritDoc}
      */
+    @Override
     public void flipPhase(long yieldTimeNsec) {
         if (!readerLock.isHeldByCurrentThread()) {
             throw new IllegalStateException("flipPhase() can only be called while holding the readerLock()");
@@ -155,19 +108,19 @@ public class StripedWriterReaderPhaser {
         if (nextPhaseIsEven) {
             initialStartValue = 0;
             for (int i=0; i<stripes; i++) {
-                evenEndEpoch.lazySet(i, initialStartValue);
+                evenEndEpoch.lazySet(i * STRIDE, initialStartValue);
             }
         } else {
             initialStartValue = Long.MIN_VALUE;
             for (int i=0; i<stripes; i++) {
-                oddEndEpoch.lazySet(i, initialStartValue);
+                oddEndEpoch.lazySet(i * STRIDE, initialStartValue);
             }
         }
 
         // Next, reset start value, indicating new phase, and retain value at flip:
         final long[] startValueAtFlip = new long[stripes];
         for (int i=0; i<stripes; i++) {
-            startValueAtFlip[i] = startEpoch.getAndSet(i, initialStartValue);
+            startValueAtFlip[i] = startEpoch.getAndSet(i * STRIDE, initialStartValue);
         }
 
         // Now, spin until previous phase end value catches up with start value at flip:
@@ -189,7 +142,7 @@ public class StripedWriterReaderPhaser {
 
     private boolean caughtUp(final long[] startValueAtFlip, final AtomicLongArray endEpoch) {
         for (int i=0; i<stripes; i++) {
-            if (endEpoch.get(i) < startValueAtFlip[i]) {
+            if (endEpoch.get(i * STRIDE) < startValueAtFlip[i]) {
                 return false;
             }
         }
@@ -199,25 +152,13 @@ public class StripedWriterReaderPhaser {
 
     private int threadIndex() {
         final int index = (int) Thread.currentThread().getId();
-        return index & (stripes - 1);
+        return STRIDE * (index & (stripes - 1));
     }
 
     /**
-     * Flip a phase in the {@link StripedWriterReaderPhaser} instance, {@link StripedWriterReaderPhaser#flipPhase()}
-     * can only be called while holding the readerLock().
-     * {@link StripedWriterReaderPhaser#flipPhase()} will return only after all writer critical sections (protected by
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} ()} and
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionExit(long)} ()}) that may have been in flight when the
-     * {@link StripedWriterReaderPhaser#flipPhase()} call were made had completed.
-     * <p>
-     * No actual writer critical section activity is required for {@link StripedWriterReaderPhaser#flipPhase()} to
-     * succeed.
-     * <p>
-     * However, {@link StripedWriterReaderPhaser#flipPhase()} is lock-free with respect to calls to
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionEnter()} and
-     * {@link StripedWriterReaderPhaser#writerCriticalSectionExit(long)}. It may spin-wait for for active
-     * writer critical section code to complete.
+     * {@inheritDoc}
      */
+    @Override
     public void flipPhase() {
         flipPhase(0);
     }
